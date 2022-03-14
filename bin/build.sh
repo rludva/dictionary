1d277cd74ed6278c63798476c1f3b1e0dc5c287d
Radomir Ludva <noreplay@radmi.cz>
2022-03-14 18:55:55 +0100

    Add basic build script..

---
+++ bin/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OUTPUT_DIRECTORY="./output"
+if [ ! -e "$OUTPUT_DIRECTORY" ]; then
+  mkdir $OUTPUT_DIRECTORY
+fi
+
+function build() {
+	GOOS="$1"
+	GOARCH="$2"
+
+	echo "Building for $GOOS/$GOARCH.."
+
+	OUTPUT="$OUTPUT_DIRECTORY/dictionary_${GOOS}_${GOARCH}"
+  if [ -e "$OUTPUT" ]; then
+		rm "$OUTPUT"
+  fi
+
+	env GOOS="$GOOS" GOARCH="$GOARCH" go build -o "$OUTPUT"
+}
+
+build linux amd64
+build windows amd64
+build darwin amd64 
+ls -l "$OUTPUT_DIRECTORY"
