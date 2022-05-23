6c8e62281f7fcd367cf40d75e7da0ac7f0808aa5
Radomir Ludva <noreplay@radmi.cz>
2022-05-24 00:10:13 +0200

    Add language package for language specific functions..

---
+++ src/language/language.go
@@ -0,0 +1,5 @@
+package language
+
+func NoAccent(input string) string {
+  return "dobry den"
+}
---
+++ src/language/language_test.go
@@ -0,0 +1,21 @@
+package language
+
+import "testing"
+
+func TestNoAccent(t *testing.T) {
+  type testCase struct {
+    input, expectedOutput string
+  }
+  testCases := []testCase {
+    {
+      "dobrý den",
+      "dobry den",
+    },
+  }
+
+  for _, tc := range testCases {
+		if got := NoAccent(tc.input); got != tc.expectedOutput {
+			t.Errorf("NoAccent(`%v`): `%v`, expected `%v`", tc.input, got, tc.expectedOutput)
+		}
+	}
+}
